5b1681fd8f483c3dde9c65d8e2384d42f271b2e1
Vic <danteckj@gmail.com>
2021-09-15 13:45:05 +0300

    добвил в опрос пустую строку и привел ответ к числу  gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,8 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: null,
+    actors: null,
+    privat: false,
+}
