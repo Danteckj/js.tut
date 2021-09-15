ed281643ca44923a715a09f32b6cf3065defe177
Vic <danteckj@gmail.com>
2021-09-15 13:56:36 +0300

    добвил добавил дб и вывод дб в консоль gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,8 +1,22 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: null,
-    actors: null,
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false,
 }
+
+const answer = prompt('Один из просмотренных Фильмов', ''),
+    answer2 = prompt('на сколько цените его?', ''),
+    answerA = prompt('Один из просмотренных Фильмов', ''),
+    answerA2 = prompt('на сколько цените его?', '');
+
+
+personalMovieDB.movies = {
+    answer: answer2,
+    answerA: answerA2,
+}
+
+console.log(personalMovieDB);
